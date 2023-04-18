744bd31f99ac4f15127f06a08d9f37595b46fe38
qa-jsban-1-aklu@example.com <qa-jsban-1-aklu@example.com>
2023-04-18 15:14:53 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16054.stoplight-dev.com/api/v1/projects/cHJqOjUzNg/spectral.js?branch_slug=main&token=db29c887-4f50-4eca-8b1e-f1fd31634872";
+export default { extends: ruleset };
